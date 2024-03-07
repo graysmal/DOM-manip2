bfb9d70ed5609e4510065ff5b44a43b1792ae02e
graysmal <128539509+graysmal@users.noreply.github.com>
2024-03-07 15:34:38 -0600

    retrieve input data

--- javas.js
+++ javas.js
@@ -1,6 +1,14 @@
 "use strict";
 
+const input = document.querySelector("#item");
+const add_btn = document.querySelector("#add_btn");
+const list = document.querySelector("#item_list");
+
+
+add_btn.addEventListener('click', addItem);
 
 function addItem() {
-
+    let new_item = document.createElement('li');
+    new_item.textContent = input.value;
+    list.append(new_item);
 }
