06861e309ab5ed759988bed9f72bb11661fe9abf
graysmal <128539509+graysmal@users.noreply.github.com>
2024-03-11 15:33:52 -0500

    delete functionality

--- javas.js
+++ javas.js
@@ -8,7 +8,22 @@ const list = document.querySelector("#item_list");
 add_btn.addEventListener('click', addItem);
 
 function addItem() {
-    let new_item = document.createElement('li');
-    new_item.textContent = input.value;
-    list.append(new_item);
+    let new_item_text = input.value;
+    input.value = "";
+
+    let new_item_li = document.createElement('li');
+    let new_item_span = document.createElement('span');
+    let new_item_btn = document.createElement('button');
+
+    new_item_li.append(new_item_span, new_item_btn);
+    
+    new_item_span.textContent = new_item_text;
+    new_item_btn.textContent = "Delete";
+
+    new_item_btn.addEventListener('click', function() {
+        list.removeChild(new_item_li);
+    });
+
+    list.append(new_item_li);
+    input.focus();
 }
